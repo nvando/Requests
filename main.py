3767a655b8f70c8c1efca2cd79d35750ea7d1a79
nvando <nikievandorst@gmail.com>
2022-08-16 14:05:14 +0200

    First commit of minimal Flask app with  test file for pytest

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")  # http://127.0.0.1:5000/
+def index():
+    return "<p>Home, sweet home.</p>"
+
+
+@app.route("/greet/")
+def greet():
+    return "<h1>Hello, world!</h1>"
+
+
+# Dynamic routing
+@app.route("/greet/<name>")
+def greet_name(name):
+    return "<h1>Hello, {}!</h1>".format(name)
+
+
+if __name__ == "__main__":
+    app.run()
+
+# set the FLASK_APP env variable to specify how to load the app with:
+# $env:FLASK_APP="file_name"
+# then run the app with the 'flask run' command
